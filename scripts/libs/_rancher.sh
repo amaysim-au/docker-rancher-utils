58cc3e4e4915a6efdf601b662676ea63965af1b9
Mark Aduna <mark.aduna@amaysim.com.au>
2018-05-04 12:54:51 +0800

    Update script to confirm healthcheck response

--- scripts/libs/_rancher.sh
+++ scripts/libs/_rancher.sh
@@ -3,21 +3,39 @@
 function check_health() {
   local health_check_url=$1
 
-  ISOK=0
-  COUNT=0
-  while [ $ISOK -eq 0 ]; do
-    echo "Waiting on Application $health_check_url ..."
+SHORT_SLEEP=1
+LONG_SLEEP=10
+FAILURE_COUNT=0
+SUCCESS_COUNT=0
+MIN_SUCCESS_COUNT=10
+MAX_FAILURE_COUNT=5
 
-    COUNT=$[$COUNT + 1]
-    if [ $COUNT -gt 90 ]; then
-      echo "Error: Application healthcheck timeout: ${health_check_url}"
-      exit 1;
-    fi
+  while [ $SUCCESS_COUNT -lt $MIN_SUCCESS_COUNT ]; do
 
-    ISOK=`curl -i -s ${health_check_url} --max-time 5 | head -1 | grep "200 OK" | wc -l`
-    sleep 10
+  if [ $FAILURE_COUNT -gt $MAX_FAILURE_COUNT ]; then
+    echo "Error: Application healthcheck timeout: $health_check_url"
+    exit 1;
+  fi
+
+  ISOK=`curl -i -s $health_check_url --max-time 5 | head -1 | grep "200" | wc -l`
+
+  if [ $ISOK -gt 0 ]; then
+    echo "Successful response from: $health_check_url"
+    SUCCESS_COUNT=$[$SUCCESS_COUNT + 1]
+    echo "Consecutive Successful responses so far: ${SUCCESS_COUNT}"
+    sleep $SHORT_SLEEP
+  else
+    echo "Error: Application healthcheck did not respond with HTTP 200: $health_check_url , resetting success count to 0"
+    SUCCESS_COUNT=0
+    FAILURE_COUNT=$[$FAILURE_COUNT + 1]
+    echo "Number of failed curl attempts so far: ${FAILURE_COUNT}"
+    echo "Waiting on Application for ${LONG_SLEEP} secs before attempting the next curl ..."
+    sleep $LONG_SLEEP
+  fi
+
   done
 
-  echo "${health_check_url} started"
+  echo "Application started"
+
   return 0;
 }
